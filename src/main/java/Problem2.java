91fb7215433a4f92c8c755f06c85ce6e00c98872
ryanjdecker <114890996+ryanjdecker@users.noreply.github.com>
2024-03-18 11:41:04 -0700

    Updated Problem2

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,13 +1,13 @@
 public class Problem2 {
 
     // Do not change signature (function name, parameters, return type)
-    public static void bubbleSort(int[] numbers) {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - 1; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
+    public static void bubbleSort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
                 }
             }
         }
