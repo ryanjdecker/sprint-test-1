31af4911c7b426b322dcf015a3620c6689542e32
ryanjdecker <114890996+ryanjdecker@users.noreply.github.com>
2024-03-21 09:32:13 -0700

    Updated Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -8,6 +8,7 @@ public class Problem2 {
                     int temp = data[j];
                     data[j] = data[j + 1];
                     data[j + 1] = temp;
+                    data[j];
                 }
             }
         }
